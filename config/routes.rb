c616d44c69c3eabe9fa30b8b751229d72d30a9f5
Noah Maquet <noahmaquet.nm@gmail.com>
2022-01-15 17:18:33 +0100

    can't do delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete "tasks/:id", to: "tasks#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tasks
 end
